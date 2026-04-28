bc7501d2a3da6ce3cb3278de7fe001cce46e3065
MrGaris <standoffplay94@gmail.com>
2026-04-28 17:01:46 +0300

    Initial vendor

---
+++ Android.mk
@@ -0,0 +1,21 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),sea)
+
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,71a1b2aabffaad30a0ca0bb500fb0622d3054c15)
+$(call add-radio-file-sha1-checked,radio/gz.img,0dcea323d9f16fd4618b44397d1913a21f52bbc0)
+$(call add-radio-file-sha1-checked,radio/lk.img,fc559beb9526ee39d9a7d318b099a86ce0bf13c8)
+$(call add-radio-file-sha1-checked,radio/logo.img,da39a3ee5e6b4b0d3255bfef95601890afd80709)
+$(call add-radio-file-sha1-checked,radio/md1img.img,cb25c03224a1d45603bc4a0ed2cb84b7da2b026d)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,31c8a22bea4c1f538e539840707691d42c57b546)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,0f9594aa878226953ff6f5dd18c9b6a34e1acda1)
+$(call add-radio-file-sha1-checked,radio/scp.img,f9a34afed9b6345a3d1faaee9e1b9fc0c8297f26)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,6f9e228bad237c8756188efb0972236c76a3d1a4)
+$(call add-radio-file-sha1-checked,radio/sspm.img,fc319b6f1d33fe5f6134d4e1cd435d223e6f45b3)
+$(call add-radio-file-sha1-checked,radio/tee.img,f5b7f3ad7de34427e1505d77d300b1dab3c14220)
+
+endif
